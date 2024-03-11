c9cdc825edb56f4c049ec5d4460102bfa25481f0
David <david@divlogic.net>
2024-03-11 00:13:04 -0600

    add Model to window of main.tsx

--- packages/db/src/main.tsx
+++ packages/db/src/main.tsx
@@ -2,10 +2,13 @@ import { Model } from './lib/db';
 
 declare const window: {
   testModel: TestModel;
+  Model: Model;
 };
 
 class TestModel extends Model<{ text: string }> {
   store = 'test_model';
 }
+
+window.Model = Model;
 
 window.testModel = new TestModel();
