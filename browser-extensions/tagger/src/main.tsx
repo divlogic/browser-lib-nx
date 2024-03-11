a2ef805f39cddb4a64e609dee2fe03c471ebc114
David <david@divlogic.net>
2024-03-11 10:10:31 -0600

    add handling for different environments

--- browser-extensions/tagger/src/main.tsx
+++ browser-extensions/tagger/src/main.tsx
@@ -3,14 +3,18 @@ import * as ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './app/app';
+const el = document.getElementById('root') as HTMLElement;
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </StrictMode>
-);
+if (el) {
+  const root = ReactDOM.createRoot(el);
+
+  root.render(
+    <StrictMode>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </StrictMode>
+  );
+} else {
+  console.error('root element not found');
+}
