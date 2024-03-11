19eb40593bb1f407b97d7af976c9d85cd5bd6174
David <david@divlogic.net>
2024-03-11 12:46:28 -0600

    add logic to initialize db store in main.tsx

--- browser-extensions/tagger/src/main.tsx
+++ browser-extensions/tagger/src/main.tsx
@@ -3,18 +3,37 @@ import * as ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './app/app';
-const el = document.getElementById('root') as HTMLElement;
+import { tag } from './db';
+window.tag = tag;
+function initializeReact() {
+  const el = document.getElementById('root') as HTMLElement;
 
-if (el) {
-  const root = ReactDOM.createRoot(el);
+  if (el) {
+    const root = ReactDOM.createRoot(el);
 
-  root.render(
-    <StrictMode>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </StrictMode>
-  );
-} else {
-  console.error('root element not found');
+    root.render(
+      <StrictMode>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </StrictMode>
+    );
+  } else {
+    console.error('root element not found');
+  }
 }
+
+async function initializeDB() {
+  const db = await tag.getDB();
+  db.close();
+  if (db.objectStoreNames.contains(tag.store)) {
+    console.log('contains it');
+    initializeReact();
+  } else {
+    console.log('doesnt contain');
+    await tag.createStore();
+    initializeReact();
+  }
+}
+
+initializeDB();
