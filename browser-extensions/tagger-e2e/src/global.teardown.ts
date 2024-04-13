fbeeab44fec67af9d97d7dae363736095c464e17
David <david@divlogic.net>
2024-04-13 15:45:22 -0600

    update global teardown to not work for indexeddb config

--- browser-extensions/tagger-e2e/src/global.teardown.ts
+++ browser-extensions/tagger-e2e/src/global.teardown.ts
@@ -1,16 +1,18 @@
 import { test as teardown } from './fixtures';
 import { TaggerDevPage } from './tagger-dev-page';
 
-teardown('clear browser.storage.local', async ({ page, extensionId }) => {
-  console.log('clearing browser.storage.local...');
-  const tagger = new TaggerDevPage(page, extensionId);
-  await tagger.goto();
+if (process.env.STORAGE_TYPE === 'browser.storage.local') {
+  teardown('clear browser.storage.local', async ({ page, extensionId }) => {
+    console.log('clearing browser.storage.local...');
+    const tagger = new TaggerDevPage(page, extensionId);
+    await tagger.goto();
 
-  await page.evaluate(() => {
-    try {
-      return window.tag.clear();
-    } catch (e) {
-      return e;
-    }
+    await page.evaluate(() => {
+      try {
+        return window.tag.clear();
+      } catch (e) {
+        return e;
+      }
+    });
   });
-});
+}
