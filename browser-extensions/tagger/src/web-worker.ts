3ba587fbe86b0f22da45fbb6d2f38e3712d41145
David <david@divlogic.net>
2024-05-18 01:42:51 -0600

    add configuration for worker that is independent of main app

--- browser-extensions/tagger/src/web-worker.ts
+++ browser-extensions/tagger/src/web-worker.ts
@@ -1,1 +1,33 @@
-console.log('web-worker.ts');
+import browser from 'webextension-polyfill';
+browser.storage.local.get('tags').then((tags) => {
+  console.log('Does this have storage?');
+  console.log(tags);
+});
+browser.runtime.onInstalled.addListener(async () => {
+  console.log('onInstalled');
+  browser.contextMenus.create({
+    id: 'testId',
+    title: 'this is title',
+    type: 'normal',
+    contexts: ['selection'],
+  });
+  browser.contextMenus.onClicked.addListener(async (info) => {
+    if (typeof info.selectionText === 'string') {
+      const task = new CreateTag(info.selectionText, 'yellow');
+      await task.run();
+    }
+  });
+});
+
+class CreateTag {
+  text: string;
+  color: string;
+  constructor(text: string, color: string) {
+    this.text = text;
+    this.color = color;
+  }
+
+  run() {
+    console.log('Ran CreateTag');
+  }
+}
