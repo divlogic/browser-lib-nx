f073c75671a7597ddd49ec2c07187a899212ff50
David <david@divlogic.net>
2024-05-30 10:50:56 -0600

    wip add test for generateAccessibleColor

--- browser-extensions/tagger/src/tests/color-utils.test.ts
+++ browser-extensions/tagger/src/tests/color-utils.test.ts
@@ -1,16 +1,27 @@
 import Color from 'colorjs.io';
-import { generateAccessibleColors } from '../color-utils';
+import {
+  generateAccessibleColor,
+  generateAccessibleColors,
+} from '../color-utils';
 
-describe('generateAccessibleColors', () => {
-  it('should return an array of accessible colors based on a starting color', () => {
-    const startingColorString = 'black';
-    const startingColor = new Color(startingColorString);
-    const colors = generateAccessibleColors(startingColorString);
-
-    colors.forEach((backgroundColor) => {
-      const bgColor = new Color(backgroundColor);
-      const rating = bgColor.contrastAPCA(startingColor);
-      expect(rating).toBeGreaterThan(90);
-    });
-  });
+describe('generateAccessibleColor', () => {
+  const startingColorString = 'black';
+  const startingColor = new Color(startingColorString);
+  const color = generateAccessibleColor(startingColorString);
+  const rating = color.contrastAPCA(startingColor);
+  expect(rating).toBeGreaterThan(90);
 });
+
+// describe('generateAccessibleColors', () => {
+//   it('should return an array of accessible colors based on a starting color', () => {
+//     const startingColorString = 'black';
+//     const startingColor = new Color(startingColorString);
+//     const colors = generateAccessibleColors(startingColorString);
+
+//     colors.forEach((backgroundColor) => {
+//       const bgColor = new Color(backgroundColor);
+//       const rating = bgColor.contrastAPCA(startingColor);
+//       expect(rating).toBeGreaterThan(90);
+//     });
+//   });
+// });
