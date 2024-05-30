ee78fc9936b2e77015b5e922000f852568e00c24
David <david@divlogic.net>
2024-05-30 11:45:35 -0600

    wip add test for cycle fn

--- browser-extensions/tagger/src/tests/color-utils.test.ts
+++ browser-extensions/tagger/src/tests/color-utils.test.ts
@@ -2,14 +2,42 @@ import Color from 'colorjs.io';
 import {
   generateAccessibleColor,
   generateAccessibleColors,
+  cycle,
 } from '../color-utils';
 
+describe('cycle', () => {
+  it('should cycle through numbers, not going over the cap', () => {
+    const start = 20;
+    const cap = 20;
+    const toAdd = 1;
+
+    expect(cycle(start, cap, toAdd)).toEqual(1);
+  });
+});
+
 describe('generateAccessibleColor', () => {
-  const startingColorString = 'black';
-  const startingColor = new Color(startingColorString);
-  const color = generateAccessibleColor(startingColorString);
-  const rating = color.contrastAPCA(startingColor);
-  expect(rating).toBeGreaterThan(90);
+  it('should generate an accessible background color from a starting color', () => {
+    const startingColorString = 'black';
+    const startingColor = new Color(startingColorString);
+    const color = generateAccessibleColor(startingColorString);
+    const rating = color.contrastAPCA(startingColor);
+    expect(rating).toBeGreaterThan(90);
+  });
+
+  it('should still work with light colors', () => {
+    const startingColorString = 'white';
+    const startingColor = new Color(startingColorString);
+    const color = generateAccessibleColor(startingColorString);
+    const rating = color.contrastAPCA(startingColor);
+    expect(Math.abs(rating)).toBeGreaterThan(90);
+  });
+  it('should still work with basic colors', () => {
+    const startingColorString = 'green';
+    const startingColor = new Color(startingColorString);
+    const color = generateAccessibleColor(startingColorString);
+    const rating = color.contrastAPCA(startingColor);
+    expect(Math.abs(rating)).toBeGreaterThan(90);
+  });
 });
 
 // describe('generateAccessibleColors', () => {
