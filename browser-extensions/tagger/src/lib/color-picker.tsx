9fc87aa03e1b28331d48260e35288a1215791ee9
David <david@divlogic.net>
2024-06-06 19:14:25 -0600

    wip add color-picker details

--- browser-extensions/tagger/src/lib/color-picker.tsx
+++ browser-extensions/tagger/src/lib/color-picker.tsx
@@ -1,6 +1,20 @@
-import { FormControl, FormLabel, Switch, Text, VStack } from '@chakra-ui/react';
-import { useState } from 'react';
+import Color from 'colorjs.io';
+import {
+  Alert,
+  AlertIcon,
+  FormControl,
+  FormLabel,
+  Input,
+  Switch,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import { useMemo, useState } from 'react';
 import { TwitterPicker } from 'react-color';
+import {
+  generateAccessibleColor,
+  generateRandomAccessibleColor,
+} from '../color-utils';
 
 /* eslint-disable-next-line */
 export interface ColorPickerProps {
@@ -14,20 +28,51 @@ export function ColorPicker({
   label = 'Color Picker',
   startingColor = 'black',
   defaultPresets = true,
-  defaultColors = [],
 }: ColorPickerProps) {
   const [presets, setPresets] = useState(defaultPresets);
   const [value, setValue] = useState('');
+  const defaultColors: string[] = useMemo(() => {
+    const colors = [0, 30, 45, 60, 100, 210, 270, 320];
+    return colors.map((hue) => {
+      const bgColor = generateAccessibleColor(startingColor, hue);
+      if (bgColor === null) {
+        return null;
+      }
+      const bgColorString = bgColor.to('srgb').toString({ format: 'hex' });
+      return bgColorString;
+    });
+  }, []).filter((color) => {
+    return color !== null;
+  });
+
   return (
     <FormControl>
       <VStack>
         <FormLabel>{label}</FormLabel>
-        <TwitterPicker
-          triangle="hide"
-          onChangeComplete={(color) => {
-            setValue(color.hex);
-          }}
-        ></TwitterPicker>
+        <Text>Current value: {value}</Text>
+        {presets ? (
+          defaultColors.length > 1 ? (
+            <TwitterPicker
+              triangle="hide"
+              colors={defaultColors}
+              onChangeComplete={(color) => {
+                setValue(color.hex);
+              }}
+            ></TwitterPicker>
+          ) : (
+            <Alert status="warning">
+              <AlertIcon /> Could not generate presets. Trying a darker or
+              lighter color should give better results.
+            </Alert>
+          )
+        ) : (
+          <Input
+            onChange={(e) => {
+              e.preventDefault();
+              setValue(e.target.value);
+            }}
+          ></Input>
+        )}
         <VStack>
           <Text>Manual | Presets</Text>
           <Switch
