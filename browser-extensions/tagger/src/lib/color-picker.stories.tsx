827162fdcf51702dfece8d53b5255f77b17680e9
David <david@divlogic.net>
2024-06-10 20:12:16 -0600

    add text to stories

--- browser-extensions/tagger/src/lib/color-picker.stories.tsx
+++ browser-extensions/tagger/src/lib/color-picker.stories.tsx
@@ -12,17 +12,21 @@ export default meta;
 type Story = StoryObj<typeof ColorPicker>;
 
 export const Black = {
+  name: 'Black startingColor',
   args: { label: 'A color picker story', startingColor: 'black' },
 };
 
 export const White: Story = {
+  name: 'White startingColor',
   args: { startingColor: 'white' },
 };
 
 export const Green: Story = {
+  name: 'Green startingColor',
   args: { startingColor: 'green' },
 };
 
 export const Yellow: Story = {
+  name: 'Yellow startingColor',
   args: { startingColor: 'yellow' },
 };
