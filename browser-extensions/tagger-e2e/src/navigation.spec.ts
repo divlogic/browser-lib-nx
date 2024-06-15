cbb479b28fa36435ad99bb028289f88d7efcd793
David <david@divlogic.net>
2024-06-14 21:15:05 -0600

    add test that navigation to styles form is possible

--- browser-extensions/tagger-e2e/src/navigation.spec.ts
+++ browser-extensions/tagger-e2e/src/navigation.spec.ts
@@ -15,7 +15,29 @@ test.describe('There should be some basic navigation tools', () => {
     });
     await expect(tagsTab).toBeVisible();
     await expect(page).toHaveTitle('Tagger');
-    const addTagForm = page.getByRole('textbox', { name: 'Add tag:' });
-    await expect(addTagForm).toBeVisible();
+    const addTagInput = page.getByRole('textbox', { name: 'Add tag:' });
+    await expect(addTagInput).toBeVisible();
+  });
+
+  test('Clicking the styles tab should present you with the styles form', async ({
+    page,
+    tagger,
+  }) => {
+    await tagger.goto();
+    let tagsTab = await page.getByRole('tab', {
+      name: 'styles',
+      selected: false,
+    });
+    await expect(tagsTab).toBeVisible();
+    await tagsTab.click();
+    tagsTab = await page.getByRole('tab', {
+      name: 'styles',
+      selected: true,
+    });
+    await expect(tagsTab).toBeVisible();
+    const heading = page.getByRole('heading', { name: 'styles' });
+    await expect(heading).toBeVisible();
+    const textColorInput = page.getByRole('textbox', { name: 'Text Color' });
+    await expect(textColorInput).toBeVisible();
   });
 });
