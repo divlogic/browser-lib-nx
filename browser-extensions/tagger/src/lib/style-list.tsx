fecd9abb3d29aaa6e900c4ddef49ee2c8af4cb10
David <david@divlogic.net>
2024-06-16 02:23:48 -0600

    wip add style-list

---
+++ browser-extensions/tagger/src/lib/style-list.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable-next-line */
+import {
+  Container,
+  Heading,
+  List,
+  ListItem,
+  OrderedList,
+  Text,
+  UnorderedList,
+} from '@chakra-ui/react';
+import { HighlightSchema, HighlightStyle } from '../schemas/style-schemas';
+
+export interface StyleListProps {
+  styles: HighlightStyle[];
+}
+
+export function StyleList({ styles }: StyleListProps) {
+  return (
+    <Container>
+      <Heading m="2">current styles</Heading>
+      <UnorderedList>
+        {styles.map((style: HighlightStyle, index) => {
+          const keys = Object.keys(
+            HighlightSchema.parse(style)
+          ) as (keyof HighlightStyle)[];
+          keys.forEach((key: keyof HighlightStyle) => {
+            const item = style[key];
+            if (Array.isArray(item)) {
+              style[key] = item.join(' ');
+            }
+          });
+          return (
+            <ListItem key={index}>
+              <Text {...style} width={'min-content'}>
+                {style.name}
+              </Text>
+            </ListItem>
+          );
+        })}
+      </UnorderedList>
+    </Container>
+  );
+}
+
+export default StyleList;
