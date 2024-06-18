64204bfe7ffe767983a1e9f48176f3c871b7e5db
David <david@divlogic.net>
2024-06-18 00:55:19 -0600

    add tests for styles.spec.ts

--- browser-extensions/tagger-e2e/src/styles.spec.ts
+++ browser-extensions/tagger-e2e/src/styles.spec.ts
@@ -8,23 +8,7 @@ const styleTagId = '#styled-by-tagger';
 // This might change from time to time in the early stages.
 test.describe('This is a test', () => {
   test.beforeEach(async ({ page, tagger }) => {
-    console.log('Navigating to styles tab');
-    await tagger.goto();
-    let tagsTab = await page.getByRole('tab', {
-      name: 'styles',
-      selected: false,
-    });
-    await expect(tagsTab).toBeVisible();
-    await tagsTab.click();
-    tagsTab = page.getByRole('tab', {
-      name: 'styles',
-      selected: true,
-    });
-    await expect(tagsTab).toBeVisible();
-    const heading = page.getByRole('heading', { name: 'styles', exact: true });
-    await expect(heading).toBeVisible();
-    const textColorInput = page.getByRole('textbox', { name: 'Text Color' });
-    await expect(textColorInput).toBeVisible();
+    tagger.gotoStyleTab();
   });
 
   test('added styles persist', async ({ page, tagger }) => {
@@ -46,6 +30,34 @@ test.describe('This is a test', () => {
     const saveButton = page.getByRole('button', { name: 'Save' });
 
     await saveButton.click();
+
+    await expect(page.getByText('test Name').first()).toBeVisible();
+  });
+
+  test('added styles persist after reload', async ({ page, tagger }) => {
+    const nameInput = page.getByRole('textbox', {
+      name: 'Name of the style',
+    });
+    await nameInput.fill('test Name');
+
+    const textColorInput = page.getByRole('textbox', {
+      name: 'Text Color',
+    });
+    textColorInput.fill('white');
+
+    const highlightColorInput = page.getByRole('textbox', {
+      name: 'Highlight Color',
+    });
+    highlightColorInput.fill('orange');
+
+    const saveButton = page.getByRole('button', { name: 'Save' });
+
+    await saveButton.click();
+
+    await expect(page.getByText('test Name').first()).toBeVisible();
+
+    await page.reload();
+    await tagger.gotoStyleTab();
 
     await expect(page.getByText('test Name').first()).toBeVisible();
   });
