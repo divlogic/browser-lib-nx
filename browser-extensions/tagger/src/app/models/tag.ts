e8756f0831eca235d43818d9a2a0c6d1a4cfe272
David <david@divlogic.net>
2024-06-22 13:31:44 -0600

    add getWithStyle fn to Tag model

--- browser-extensions/tagger/src/app/models/tag.ts
+++ browser-extensions/tagger/src/app/models/tag.ts
@@ -1,8 +1,26 @@
 import { StoreModel } from '../../db/store-model';
+import { HighlightStyle } from '../../schemas';
 import { TagType } from '../../schemas/tag-schemas';
-export class TagModel extends StoreModel<TagType> {
-  public static key = 'tags';
-  ['constructor']!: typeof TagModel & typeof StoreModel;
+import { styleModel } from './style';
+export class Tag extends StoreModel<TagType> {
+  public static key = 'tag';
+  ['constructor']!: typeof Tag & typeof StoreModel;
+
+  async getWithStyle(): Promise<Tag[]> {
+    const tags = await this.get();
+    console.log('tags from getWithStyle:', tags);
+
+    const styles: { [key: string]: HighlightStyle } = {};
+    (await styleModel.get())?.forEach((style) => {
+      if (style.name) {
+        styles[style.name] = style;
+      }
+    });
+    const tagsWithStyle = tags?.map((tag) => {
+      return (tag.style = styles[tag.style_name]);
+    });
+    return tagsWithStyle;
+  }
 }
 
-export const tagModel = new TagModel();
+export const tagModel = new Tag();
