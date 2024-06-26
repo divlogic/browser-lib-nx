5d41d4dd2162fe77d41564815113c09350ae1f99
David <david@divlogic.net>
2024-06-25 21:11:04 -0600

    add TagsProvider to app

--- browser-extensions/tagger/src/app/app.tsx
+++ browser-extensions/tagger/src/app/app.tsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 import { useEffect, useReducer } from 'react';
-import { tagsReducer } from './tags-reducer';
+import { tagsReducer } from './tags-reducer.OLD';
 import { HighlightTags } from '../tagger';
 import { tagModel } from './models';
 import MainPage from './pages/main-page';
 import StylesPage from './pages/styles-page';
-import { StylesProvider } from './providers';
+import { StylesProvider, TagsProvider } from './providers';
 
 export function App() {
   const [tags, dispatch] = useReducer(tagsReducer, {
@@ -29,22 +29,24 @@ export function App() {
   }, [tags]);
 
   return (
-    <StylesProvider>
-      <Tabs orientation="vertical" variant="solid-rounded" width="100%">
-        <TabList>
-          <Tab>Tags</Tab>
-          <Tab>Styles</Tab>
-        </TabList>
-        <TabPanels width={'100%'}>
-          <TabPanel>
-            <MainPage dispatch={dispatch} tags={tags} />
-          </TabPanel>
-          <TabPanel>
-            <StylesPage />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </StylesProvider>
+    <TagsProvider>
+      <StylesProvider>
+        <Tabs orientation="vertical" variant="solid-rounded" width="100%">
+          <TabList>
+            <Tab>Tags</Tab>
+            <Tab>Styles</Tab>
+          </TabList>
+          <TabPanels width={'100%'}>
+            <TabPanel>
+              <MainPage dispatch={dispatch} tags={tags} />
+            </TabPanel>
+            <TabPanel>
+              <StylesPage />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </StylesProvider>
+    </TagsProvider>
   );
 }
 
