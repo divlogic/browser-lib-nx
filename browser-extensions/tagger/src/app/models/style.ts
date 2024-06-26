9a22f51f512fb980b8c04fcfb3cd48b1e45104a4
David <david@divlogic.net>
2024-06-26 17:53:41 -0600

    fix incorrect string literal

--- browser-extensions/tagger/src/app/models/style.ts
+++ browser-extensions/tagger/src/app/models/style.ts
@@ -2,7 +2,7 @@ import { StoreModel } from '../../db/store-model';
 import { HighlightStyle } from '../../schemas';
 export class Style extends StoreModel<HighlightStyle> {
   ['constructor']!: typeof Style & typeof StoreModel;
-  public static key = 'style';
+  public static key = 'styles';
 }
 
 export const styleModel = new Style();
