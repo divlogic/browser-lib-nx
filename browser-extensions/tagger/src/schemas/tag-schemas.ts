ff34adb5956b0f035d29be342c5202f207c23f80
David <david@divlogic.net>
2024-06-30 14:41:51 -0600

    add UnsavedTagSchema and UnsavedTagType

--- browser-extensions/tagger/src/schemas/tag-schemas.ts
+++ browser-extensions/tagger/src/schemas/tag-schemas.ts
@@ -2,10 +2,14 @@ import { z } from 'zod';
 import { HighlightSchema } from './style-schemas';
 
 export const TagSchema = z.object({
-  id: z.number().optional(),
+  id: z.number(),
   text: z.string(),
   style_name: z.string(),
   style: HighlightSchema.optional(),
 });
 
+export const UnsavedTagSchema = TagSchema.omit({ id: true });
+
 export type TagType = z.infer<typeof TagSchema>;
+
+export type UnsavedTagType = z.infer<typeof TagSchema>;
